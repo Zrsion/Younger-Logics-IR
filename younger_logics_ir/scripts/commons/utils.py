0f17f7affbde4698b8a31e185a6caf0ecd2df6af
Jason-Young-AI <AI.Jason.Young@outlook.com>
2024-12-30 09:33:50 +0800

    For Compatability about torch.onnx.export

--- younger_logics_ir/scripts/commons/utils.py
+++ younger_logics_ir/scripts/commons/utils.py
@@ -6,7 +6,7 @@
 # Author: Jason Young (杨郑鑫).
 # E-Mail: AI.Jason.Young@outlook.com
 # Last Modified by: Jason Young (杨郑鑫)
-# Last Modified time: 2024-12-29 20:03:58
+# Last Modified time: 2024-12-30 08:54:08
 # Copyright (c) 2024 Yangs.AI
 # 
 # This source code is licensed under the Apache License 2.0 found in the
@@ -18,10 +18,21 @@ import onnx
 
 
 def get_onnx_opset_versions() -> list[int]:
+    """
+    return opset version list (>=7)
+
+    .. note::
+        torch.onnx.export only supports opset version >= 7
+
+    :return: _description_
+    :rtype: list[int]
+    """
     onnx_opset_version_set: set[int] = set()
     onnx_version_table = onnx.helper.VERSION_TABLE
     for onnx_version in onnx_version_table:
         onnx_opset_version = onnx_version[2]
+        if onnx_opset_version < 7:
+            continue
         onnx_opset_version_set.add(onnx_opset_version)
 
     onnx_opset_versions: list[int] = sorted(list(onnx_opset_version_set))
