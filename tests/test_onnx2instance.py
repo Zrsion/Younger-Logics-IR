4d1d2b32b81f4a0a1765a85d02d13c0c720a6a57
Jason-Young-AI <AI.Jason.Young@outlook.com>
2025-01-07 10:10:16 +0800

    Sync

--- tests/test_onnx2instance.py
+++ tests/test_onnx2instance.py
@@ -6,7 +6,7 @@
 # Author: Jason Young (杨郑鑫).
 # E-Mail: AI.Jason.Young@outlook.com
 # Last Modified by: Jason Young (杨郑鑫)
-# Last Modified time: 2025-01-03 23:12:19
+# Last Modified time: 2025-01-07 10:09:49
 # Copyright (c) 2025 Yangs.AI
 # 
 # This source code is licensed under the Apache License 2.0 found in the
@@ -55,28 +55,33 @@ def main(load_dirpath: pathlib.Path, save_dirpath: pathlib.Path):
     for index, load_filepath in enumerate(load_dirpath.glob('*.onnx')):
         onnx_model_filename = load_filepath.name.split(".")[0]
         logger.info(onnx_model_filename)
+        logger.info(f' v Start Load')
         onnx_model = onnx.load(load_filepath, load_external_data=False)
+        logger.info(f' ^ End Load')
 
         instance = Instance()
 
         # Convert the ONNX model to YLIR instance
-        logger.info('onnx loaded')
+        logger.info(f' v Start Convert')
         dag = convert(onnx_model)
-        logger.info(1111111)
+        logger.info(f' ^ End Convert')
+
+        logger.info(f' v Start Setup Instance')
         instance.setup_logicx(dag)
-        logger.info(2222222)
         instance.insert_label(
             Implementation(
                 origin = Origin(YLIROriginHub.LOCAL, 'Test', f'{index}_{onnx_model_filename}')
             )
         )
-        logger.info(33333333)
+        logger.info(f' ^ End Setup Instance')
+
         # sd = LogicX.saved_dag(instance.logicx.dag)
 
         # find_bytes_in_dict(sd)
 
+        logger.info(f' v Start Cal Unique')
         instance_unique = instance.unique
-        logger.info(4444444444)
+        logger.info(f' ^ End Cal Unique')
         instance.save(save_dirpath.joinpath(instance_unique))
 
         save_json(LogicX.saved_dag(instance.logicx.dag), save_dirpath.joinpath(instance_unique, f'{onnx_model_filename}.json'), indent=2, cls=YLIRJSONEncoder)
