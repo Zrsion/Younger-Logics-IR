b31577574eb795599b2f9b7a2404bb6072da42d9
Jason-Young-AI <AI.Jason.Young@outlook.com>
2025-01-11 11:17:20 +0800

    Update

--- younger_logics_ir/commands/create.py
+++ younger_logics_ir/commands/create.py
@@ -6,7 +6,7 @@
 # Author: Jason Young (杨郑鑫).
 # E-Mail: AI.Jason.Young@outlook.com
 # Last Modified by: Jason Young (杨郑鑫)
-# Last Modified time: 2025-01-10 17:01:48
+# Last Modified time: 2025-01-11 11:17:12
 # Copyright (c) 2024 Yangs.AI
 # 
 # This source code is licensed under the Apache License 2.0 found in the
@@ -123,18 +123,19 @@ def create_onnx_convert():
 @click.option('--model-size-limit-l',   required=False, type=int, default=0, help='Used to filter out oversized models to prevent process interruptions due to excessive storage usage. (Note: The storage space occupied by models is a simple estimation and may have inaccuracies. Please use with caution.)')
 @click.option('--model-size-limit-r',   required=False, type=int, default=3*1024*1024*1024, help='Used to filter out oversized models to prevent process interruptions due to excessive storage usage. (Note: The storage space occupied by models is a simple estimation and may have inaccuracies. Please use with caution.)')
 @click.option('--token',                required=False, type=str, default=None, help='The HuggingFace token, which requires registering an account on HuggingFace and manually setting the access token. If None, retrieve without HuggingFace access token.')
+@click.option('--estimate',             is_flag=True,   help='Use to estimate models will be converted. No Conversion Processes.')
 @click.option('--logging-filepath',     required=False, type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=pathlib.Path), default=None, help='Path to the log file; if not provided, defaults to outputting to the terminal only.')
 def create_onnx_convert_huggingface(
     model_infos_filepath,
     save_dirpath, cache_dirpath,
-    device, framework, model_size_limit_l, model_size_limit_r, token,
+    device, framework, model_size_limit_l, model_size_limit_r, token, estimate,
     logging_filepath
 ):
     equip_logger(logging_filepath)
 
     from younger_logics_ir.scripts.hubs.huggingface import convert
 
-    convert.main(model_infos_filepath, save_dirpath, cache_dirpath, device=device, framework=framework, model_size_limit_l=model_size_limit_l, model_size_limit_r=model_size_limit_r, token=token)
+    convert.main(model_infos_filepath, save_dirpath, cache_dirpath, device=device, framework=framework, model_size_limit_l=model_size_limit_l, model_size_limit_r=model_size_limit_r, token=token, estimate=estimate)
 
 
 @create_onnx_convert.command(name='onnx')
