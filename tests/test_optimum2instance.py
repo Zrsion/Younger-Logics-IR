44492573a5134cedeff5b20620cc0a1df322cc7f
Jason-Young-AI <AI.Jason.Young@outlook.com>
2025-03-24 11:16:45 +0800

    Emmmm

--- tests/test_optimum2instance.py
+++ tests/test_optimum2instance.py
@@ -6,7 +6,7 @@
 # Author: Jason Young (杨郑鑫).
 # E-Mail: AI.Jason.Young@outlook.com
 # Last Modified by: Jason Young (杨郑鑫)
-# Last Modified time: 2025-01-10 23:34:41
+# Last Modified time: 2025-03-24 09:44:54
 # Copyright (c) 2025 Yangs.AI
 # 
 # This source code is licensed under the Apache License 2.0 found in the
@@ -19,15 +19,17 @@ import pathlib
 
 from younger.commons.io import create_dir
 
-from younger_logics_ir.modules import LogicX
-from younger_logics_ir.scripts.hubs.huggingface.convert import convert_optimum
+from younger_logics_ir.commons.constants import YLIROriginHub
+from younger_logics_ir.modules import LogicX, Implementation, Origin
+from younger_logics_ir.scripts.hubs.huggingface.convert import convert_onnx
 
-from optimum.exporters.onnx import main_export
+#from optimum.exporters.onnx import main_export
 
 
 @click.command()
 @click.option('--cache-dirpath', required=True,  type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=pathlib.Path), help='Cache directory, where data is volatile.')
-def main(cache_dirpath):
+@click.option('--print-dirpath', required=True,  type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=pathlib.Path), help='Print directory, where data is saving.')
+def main(cache_dirpath, print_dirpath):
 
     # Official
     ofc_cache_dirpath = cache_dirpath.joinpath(f'Cache-HFOfc')
@@ -37,13 +39,24 @@ def main(cache_dirpath):
     cvt_cache_dirpath = cache_dirpath.joinpath(f'Cache-HFCvt')
     create_dir(cvt_cache_dirpath)
 
-    model_id = 'hks1444/bert-base-turkish-cased2_with_categories'
+    #model_id = 'hks1444/bert-base-turkish-cased2_with_categories'
+    model_id = 'csukuangfj/sherpa-onnx-lstm-zh-2023-02-20'
     # main_export(model_id, cvt_cache_dirpath, opset=14, device='cpu', cache_dir=ofc_cache_dirpath, monolith=True, do_validation=False, trust_remote_code=True, no_post_process=True)
-    status, instances = convert_optimum(model_id, cvt_cache_dirpath, ofc_cache_dirpath, device='cpu')
+    status, instances, filenames = convert_onnx(model_id, cvt_cache_dirpath, ofc_cache_dirpath, device='cpu')
+    #status, instances = convert_optimum(model_id, cvt_cache_dirpath, ofc_cache_dirpath, device='cpu')
     print(status)
     print(instances)
-    for instance in instances:
-        print(LogicX.luid(instance.logicx))
+    print(filenames)
+    for instance, filename in zip(instances, filenames):
+        instance.insert_label(
+            Implementation(
+                origin=Origin(YLIROriginHub.HUGGINGFACE, 'csukuangfj', 'sherpa-onnx-lstm-zh-2023-02-20' + '-F-' + filename),
+                like=0,
+                download=0,
+            )
+        )
+        instance.save(print_dirpath.joinpath(f'{instance.unique}'))
+        #print(LogicX.luid(instance.logicx))
 
 
 if __name__ == '__main__':
